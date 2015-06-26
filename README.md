90835da84706904abdf585a48bb0f7611ce29c5c
Nick Baugh <niftylettuce@gmail.com>
2015-06-26 19:11:54 -0400

    Fixed tests, wrote for simplification, fixed Readme, added documentation for usage, renamed MyModel to MySchema

--- README.md
+++ README.md
@@ -1,116 +1,177 @@
 
-# mongoose-paginate [![Build Status](https://secure.travis-ci.org/edwardhotchkiss/mongoose-paginate.png)](http://travis-ci.org/edwardhotchkiss/mongoose-paginate) [![Git Tip](http://img.shields.io/gittip/edwardhotchkiss.svg)](https://www.gittip.com/edwardhotchkiss/)
+# mongoose-paginate
 
-> Mongoose ORM (NodeJS/MongoDB) Document Query Pagination
+[![NPM version][npm-image]][npm-url]
+[![NPM downloads][npm-downloads]][npm-url]
+[![Circle CI][circleci-image]][circleci-url]
+[![Static Analysis][codeclimate-image]][codeclimate-url]
+[![MIT License][license-image]][license-url]
+[![Slack][slack-image]][slack-url]
 
-To be used in combination with view pagination middleware such as [express-paginate](https://github.com/niftylettuce/express-paginate).
+> `mongoose-paginate` is a plugin for [Mongoose][mongoose] schemas to easily add paginated queries and results.  This plugin is to be used in combination with view pagination middleware such as [express-paginate](https://github.com/niftylettuce/express-paginate).
 
-## Installation
+
+## Index
+
+* [Install](#install)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+
+
+## Install
 
 ```bash
 npm install -S mongoose-paginate
 ```
 
+
 ## Usage
 
-#### Basic
+This plugin must first be added to a schema:
 
 ```js
 
-/*
- * basic example usage of `mongoose-pagination`
- * querying for `all` {} items in `MyModel`
- * paginating by second page, 10 items per page (10 results, page 2)
- */
-
 var mongoosePaginate = require('mongoose-paginate');
 
-MyModel.plugin(mongoosePaginate)
-
-MyModel.paginate({}, {page: 2, limit: 10}, function(error, result) {
-  if (error) {
-    console.error(error);
-  } else {
-  	console.log('Pages:', result.pageCount);
-    console.log(result.paginatedResults);
-  }
-});
+MySchema.plugin(mongoosePaginate);
 
 ```
 
-#### Advanced
+`MySchema` will have a new function called `paginate` (e.g. `MySchema.paginate()`).
+
+### MySchema.paginate(query, options, callback)
+
+**Arguments**
+
+* `query` - An object for the [Mongoose][mongoose] query.
+* `options` - An object with options for the [Mongoose][mongoose] query, such as sorting and population
+  - `page` - Default: `1`
+  - `limit` - Default: `10`
+  - `columns` - Default: `null`
+  - `sortBy` - Default: `null`
+  - `populate` - Default: `null`
+* `callback(err, results, pageCount, itemCount)` - A callback which is called once pagination results are retrieved, or when an error has occurred.
+
+**Examples**
 
 ```js
 
-/*
- * advanced example usage of `mongoose-pagination`
- * querying for `{ columns: 'title', { populate: 'some_ref' }, { sortBy : { title : -1 } }` items in `MyModel`
- * paginating by second page, 10 items per page (10 results, page 2)
- */
+// basic example usage of `mongoose-pagination`
+// querying for `all` {} items in `MySchema`
+// paginating by second page, 10 items per page (10 results, page 2)
 
 var mongoosePaginate = require('mongoose-paginate');
 
-MyModel.plugin(mongoosePaginate)
+MySchema.plugin(mongoosePaginate);
 
-MyModel.paginate({}, {page: 2, limit: 10, columns: 'title', populate: 'some_ref', sortBy : { title : -1 },
- function(error, result) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Pages:', result.pageCount);
-    console.log(result.paginatedResults);
-  }
-});
-
-/*
- * Populating more than one ref
- */
- 
- MyModel.paginate({}, 2, 10, function(error, pageCount, paginatedResults, itemCount) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Pages:', pageCount);
-    console.log(paginatedResults);
-  }
-}, { columns: 'title', populate: ['some_ref', 'other_ref'], sortBy : { title : -1 });
-
-/* 
- * Selecting specific field for population (http://mongoosejs.com/docs/api.html#query_Query-populate)
- */
- 
- MyModel.paginate({}, 2, 10, function(error, pageCount, paginatedResults, itemCount) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Pages:', pageCount);
-    console.log(paginatedResults);
-  }
-}, { columns: 'title', populate: [{ path: 'some_ref', select: 'field_a field_b' }, 'other_ref' ], sortBy : { title : -1 });
+MySchema.paginate({}, {
+  page: 2, limit: 10
+}, callback);
 
 ```
-##### Return object
 
-Returning object looks following:
-```json
+```js
 
-{"pageCount": "Number", "paginatedResult": "Number", "itemCount": "Number"}
+// advanced example usage of `mongoose-pagination`
+// querying for `{ columns: 'title', { populate: 'some_ref' }, { sortBy : { title : -1 } }` items in `MySchema`
+// paginating by second page, 10 items per page (10 results, page 2)
+
+MySchema.paginate(
+  {},
+  {
+    page: 2,
+    limit: 10,
+    columns: 'title',
+    populate: 'some_ref',
+    sortBy: {
+      title: -1
+    }
+  },
+  callback
+);
 
 ```
-## Run Tests
 
-``` bash
-$ npm test
+```js
+
+// populating more than one ref
+
+MySchema.paginate({}, {
+  page: 2,
+  limit: 10,
+  columns: 'title',
+  populate: [ 'some_ref', 'other_ref' ],
+  sortBy: {
+    title: -1
+  }
+}, callback);
+
 ```
 
-### Author: [Edward Hotchkiss][0]
+```js
 
-[0]: http://edwardhotchkiss.com/
+// selecting specific field for population
+// <http://mongoosejs.com/docs/api.html#query_Query-populate>
+
+MySchema.paginate({}, {
+  columns: 'title',
+  populate: [
+    {
+      path: 'some_ref',
+      select: 'field_a field_b'
+    },
+    'other_ref'
+  ],
+  sortBy: {
+    title: -1
+  }
+}, callback);
+
+```
 
 
-### Contributors
+## Tests
 
-* [Nick Baugh](https://github.com/niftylettuce)
+```bash
+npm test
+```
 
-[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/edwardhotchkiss/mongoose-paginate/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
 
+## Contributors
+
+* Edward Hotchkiss <edwardhotchkiss@me.com>
+* Nick Baugh <niftylettuce@gmail.com>
+* villesau <ville.saukkonen@sulake.com>
+* Danilo Barsotti <danilo.barsotti@musiclize.com>
+* t_yamo <t_yamo@unknown-artifacts.info>
+* andrew <andrew@andrew-desktop.(none)>
+* Alberto Gimeno Brieba <gimenete@gmail.com>
+* zhiqingchen <zhiqingchen@anjuke.com>
+* Alexander Manzyuk <admsev@gmail.com>
+* Gia <anggiaj@users.noreply.github.com>
+* Hrvoje Šimić <shime.ferovac@gmail.com>
+* Mario Colque <mario@feegos.com>
+* Richard van der Dys <rhvanderdys@containerstore.com>
+* Yolanda Septiana <yolapopop@gmail.com>
+* charles bourasseau <charles.bourasseau@gmail.com>
+* giulianoiacobelli <giuliano.iacobelli@gmail.com>
+
+
+## License
+
+[MIT][license-url]
+
+
+[mongoose]: http://mongoosejs.com
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg?style=flat
+[license-url]: LICENSE
+[codeclimate-image]: http://img.shields.io/codeclimate/github/edwardhotchkiss/mongoose-paginate.svg?style=flat
+[codeclimate-url]: https://codeclimate.com/github/edwardhotchkiss/mongoose-paginate
+[npm-image]: http://img.shields.io/npm/v/mongoose-paginate.svg?style=flat
+[npm-url]: https://npmjs.org/package/mongoose-paginate
+[npm-downloads]: http://img.shields.io/npm/dm/mongoose-paginate.svg?style=flat
+[circleci-image]: https://circleci.com/gh/edwardhotchkiss/mongoose-paginate/tree/master.svg?style=svg
+[circleci-url]: https://circleci.com/gh/edwardhotchkiss/mongoose-paginate/tree/master
+[slack-url]: http://slack.eskimo.io/
+[slack-image]: http://slack.eskimo.io/badge.svg
--- test/all.test.js
+++ test/all.test.js
@@ -1,30 +1,25 @@
 'use strict';
-/*
- * @list dependencies
- */
 
-var vows = require('vows')
-  , assert = require('chai').assert
-  , mongoose = require('mongoose')
-  , mongoosePaginate = require('../lib/mongoose-paginate');
+var vows = require('vows');
+var assert = require('chai').assert;
+var mongoose = require('mongoose');
+var mongoosePaginate = require('../lib/mongoose-paginate');
 
-/*
- * connect to MongoDB with Mongoose
- */
+// connect to MongoDB with Mongoose
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/mongoose_paginate_test');
 
-mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/mongoosePaginateTest');
-
-/*
- * @tests setup
- */
+// test setup
 var testEntriesCollectionName = 'mongoosePaginateTestEntries';
 var testEntriesSubCollectionName = 'mongoosePaginateTestSubEntries';
 
 var TestSchema = new mongoose.Schema({
-  id    : mongoose.Schema.ObjectId,
-  title : String,
-  date  : Date,
-  child : { type: mongoose.Schema.ObjectId, ref: testEntriesSubCollectionName }
+  id: mongoose.Schema.ObjectId,
+  title: String,
+  date: Date,
+  child: {
+    type: mongoose.Schema.ObjectId,
+    ref: testEntriesSubCollectionName
+  }
 });
 
 TestSchema.plugin(mongoosePaginate);
@@ -32,9 +27,9 @@ TestSchema.plugin(mongoosePaginate);
 var TestEntry = mongoose.model(testEntriesCollectionName, TestSchema);
 
 var TestSubSchema = new mongoose.Schema({
-  id    : mongoose.Schema.ObjectId,
-  title : String,
-  date  : Date
+  id: mongoose.Schema.ObjectId,
+  title: String,
+  date: Date
 });
 
 TestSubSchema.plugin(mongoosePaginate);
@@ -45,194 +40,171 @@ function setup(callback) {
   var newSubEntry = new TestSubEntry({
     title: 'SubItem #1'
   });
-  newSubEntry.save(function(error, subEntry) {
+  newSubEntry.save(function(err) {
+    if (err) return callback(err);
+    var testEntries = [];
     for (var i=1; i<=100;i++) {
       var newEntry = new TestEntry({
-        title : 'Item #'+i,
-        child : subEntry._id
+        title: 'Item #' + i,
+        child: newSubEntry._id
       });
-      newEntry.save(increment(i, callback));
+      testEntries.push(newEntry);
     }
+    TestEntry.create(testEntries, callback);
   });
 }
 
-/*
- * teardown
- */
-
-function teardown(callback){
-  mongoose.connection.collections[testEntriesSubCollectionName.toLowerCase()].drop(function(err) {
-    if(err) console.log(err);
-    mongoose.connection.collections[testEntriesCollectionName.toLowerCase()].drop(callback);
-  });
+function teardown(callback) {
+  mongoose.connection.db.dropDatabase(callback);
 }
-
-function increment(complete, callback) {
-  return function(error) {
-    if (error) {
-      console.error(error);
-    } else {
-      if (complete === 100) {
-        callback(null, 100);
-      }
-    }
-  };
-}
-
-/*
- * @tests vows
- */
 
 vows.describe('pagination module basic tests')
-
-.addBatch({
-  'when requiring `mongoose-paginate`':{
-    topic:function(){
-      return mongoose;
-    },
-    'there should be no errors and paginate should be an object':function(topic) {
-      assert.equal(typeof(topic), 'object');
+  .addBatch({
+    'when requiring `mongoose-paginate`':{
+      topic: function() {
+        return mongoose;
+      },
+      'there should be no errs and paginate should be an object': function(topic) {
+        assert.equal(typeof(topic), 'object');
+      }
     }
-  }
-})
+  })
 
-.addBatch({
-  'teardown the collections before running tests':{
-    topic:function(){
-      teardown(this.callback);
+  .addBatch({
+    'teardown the collections before running tests':{
+      topic: function() {
+        teardown(this.callback);
+      }
     }
-  }
-})
-
-.addBatch({
-  'when creating 100 dummy documents with our test mongodb string':{
-    topic:function(){
-      setup(this.callback);
-    },
-    'there should be no errors and resultCount should be 100':function(error, resultCount) {
-      assert.equal(error, null);
-      assert.equal(resultCount, 100);
+  })
+  .addBatch({
+    'when creating 100 dummy documents with our test mongodb string':{
+      topic: function() {
+        setup(this.callback);
+      },
+      'there should be no errs and documents.length should be 100': function(err, documents) {
+        assert.equal(err, null);
+        assert.equal(documents.length, 100);
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating TestEntry querying for all documents, with page 1, 10 results per page':{
-    topic:function(){
-      TestEntry.paginate({}, {page: 1, limit: 10, columns: 'title'}, this.callback);
-    },
-    'there should be no errors':function(error, results) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(1)':function(error, result) {
-      assert.equal(result.paginatedResults[0].title, 'Item #1');
+  })
+  .addBatch({
+    'when paginating TestEntry querying for all documents, with page 1, 10 results per page':{
+      topic: function() {
+        TestEntry.paginate({}, { page: 1, limit: 10, columns: 'title' }, this.callback);
+      },
+      'there should be no errs': function(err, results) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(1)': function(err, results) {
+        assert.equal(results[0].title, 'Item #1');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating without page and limit, use default values 1 and 10':{
-    topic:function(){
-      TestEntry.paginate({}, {}, this.callback);
-    },
-    'there should be no errors':function(error, result) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(1)':function(error, result) {
-      assert.equal(result.paginatedResults[0].title, 'Item #1');
+  })
+  .addBatch({
+    'when paginating without page and limit, use default values 1 and 10':{
+      topic: function() {
+        TestEntry.paginate({}, {}, this.callback);
+      },
+      'there should be no errs': function(err, results) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(1)': function(err, results) {
+        assert.equal(results[0].title, 'Item #1');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating TestEntry querying for all documents, with page 2, 10 results per page':{
-    topic:function(){
-      TestEntry.paginate({}, {page: 2, limit: 10, columns: 'title' }, this.callback);
-    },
-    'there should be no errors':function(error, result) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(11)':function(error, result) {
-      assert.equal(result.paginatedResults[0].title, 'Item #11');
-    },
-    'there should be 100 items as results':function(error, result) {
-      assert.equal(result.itemCount, 100);
+  })
+  .addBatch({
+    'when paginating TestEntry querying for all documents, with page 2, 10 results per page':{
+      topic: function() {
+        TestEntry.paginate({}, { page: 2, limit: 10, columns: 'title' }, this.callback);
+      },
+      'there should be no errs': function(err, results) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(11)': function(err, results) {
+        assert.equal(results[0].title, 'Item #11');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating TestEntry querying for all documents, with page 10, 11 results per page':{
-    topic:function(){
-      TestEntry.paginate({}, {page: 10, limit: 10,  columns: 'title' }, this.callback);
-    },
-    'there should be no errors':function(error, result) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(100)':function(error, result) {
-      assert.equal(result.paginatedResults[9].title, 'Item #100');
+  })
+  .addBatch({
+    'when paginating TestEntry querying for all documents, with page 10, 11 results per page':{
+      topic: function() {
+        TestEntry.paginate({}, {page: 10, limit: 10,  columns: 'title' }, this.callback);
+      },
+      'there should be no errs': function(err, results) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(100)': function(err, results) {
+        assert.equal(results[9].title, 'Item #100');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating TestEntry querying for all documents, with page 2, 10 results per page with populate and without columns':{
-    topic:function(){
-      TestEntry.paginate({}, {page: 2, limit: 10,  populate: 'child'}, this.callback);
-    },
-    'there should be no errors':function(error, result) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(11)':function(error, result) {
-      assert.equal(result.paginatedResults[0].title, 'Item #11');
-    },
-    'the first result should contain the correct SubItem #(1)':function(error, result) {
-      assert.equal(result.paginatedResults[0].child.title, 'SubItem #1');
+  })
+  .addBatch({
+    'when paginating TestEntry querying for all documents, with page 2, 10 results per page with populate and without columns':{
+      topic: function() {
+        TestEntry.paginate({}, {page: 2, limit: 10,  populate: 'child'}, this.callback);
+      },
+      'there should be no errs': function(err, results) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(11)': function(err, results) {
+        assert.equal(results[0].title, 'Item #11');
+      },
+      'the first result should contain the correct SubItem #(1)': function(err, results) {
+        assert.equal(results[0].child.title, 'SubItem #1');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when paginating TestEntry querying for all documents, with page 1, 10 results per page, sorting reverse by title':{
-    topic:function(){
-      TestEntry.paginate({}, {page: 1, limit: 10, sortBy : { title : -1 } }, this.callback);
-    },
-    'there should be no errors':function(error, result) {
-      assert.equal(error, null);
-    },
-    'results.length should be 10':function(error, result) {
-      assert.equal(result.paginatedResults.length, 10);
-    },
-    'the first result should contain the correct index #(99)':function(error, result) {
-      assert.equal(result.paginatedResults[0].title, 'Item #99');
+  })
+  .addBatch({
+    'when paginating TestEntry querying for all documents, with page 1, 10 results per page, sorting reverse by title':{
+      topic: function() {
+        TestEntry.paginate(
+          {},
+          {
+            page: 1,
+            limit: 10,
+            sortBy: {
+              title: -1
+            }
+          },
+          this.callback
+        );
+      },
+      'there should be no errs': function(err, results, pageCount, itemCount) {
+        assert.equal(err, null);
+      },
+      'results.length should be 10': function(err, results, pageCount, itemCount) {
+        assert.equal(results.length, 10);
+      },
+      'the first result should contain the correct index #(99)': function(err, results, pageCount, itemCount) {
+        assert.equal(results[0].title, 'Item #99');
+      }
     }
-  }
-})
-
-.addBatch({
-  'when deleting all of our 100 dummy documents with our test mongodb string':{
-    topic:function(){
-      teardown(this.callback);
-    },
-    'there should be no errors and resultCount should be a number':function(error, result) {
-      assert.equal(error, null);
+  })
+  .addBatch({
+    'when deleting all of our 100 dummy documents with our test mongodb string':{
+      topic: function() {
+        teardown(this.callback);
+      },
+      'there should be no errs': function(dropped) {
+        assert.strictEqual(dropped, true);
+      }
     }
-  }
-})
-
-.export(module);
+  })
+  .export(module);
