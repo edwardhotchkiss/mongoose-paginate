4d256fe09247eb72cbd35424dc2fc0c645b34085
unknown <davico788@hotmail.com>
2020-02-01 11:26:30 -0300

    replate estimated documento to count documents

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function paginate(query, options, callback) {
     }
     promises = {
       docs: docsQuery.exec(),
-      count: this.estimatedDocumentCount(query).exec()
+      count: this.countDocuments(query)
     };
     if (lean && leanWithId) {
       promises.docs = promises.docs.then((docs) => {
